369c876d432198a86c8e32609b93af72b08134ea
Austin King <shout@ozten.com>
2022-12-06 08:35:40 -0800

    Part 1 refactor

--- day_06_tuning_trouble/src/main.rs
+++ day_06_tuning_trouble/src/main.rs
@@ -3,15 +3,12 @@ use std::fs::read_to_string;
 
 use std::error::Error;
 
-const WINDOW_SIZE: usize = 4;
-
-fn detect_signal(transmission: String) -> usize {
-    let window_size = WINDOW_SIZE;
+fn detect_signal(transmission: String, window_size: usize) -> usize {
     let mut idx = 0;
     while idx + window_size < transmission.len() {
-        let window: HashSet<char> = transmission[idx..idx + WINDOW_SIZE].chars().collect();
-        if window.len() == WINDOW_SIZE {
-            return idx + WINDOW_SIZE;
+        let window: HashSet<char> = transmission[idx..idx + window_size].chars().collect();
+        if window.len() == window_size {
+            return idx + window_size;
         }
 
         idx += 1;
@@ -22,28 +19,28 @@ fn detect_signal(transmission: String) -> usize {
 #[test]
 fn test_detect_signal() {
     assert_eq!(
-        detect_signal(String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb")),
+        detect_signal(String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 4),
         7
     );
     assert_eq!(
-        detect_signal(String::from("bvwbjplbgvbhsrlpgdmjqwftvncz")),
+        detect_signal(String::from("bvwbjplbgvbhsrlpgdmjqwftvncz"), 4),
         5
     );
     assert_eq!(
-        detect_signal(String::from("nppdvjthqldpwncqszvftbrmjlhg")),
+        detect_signal(String::from("nppdvjthqldpwncqszvftbrmjlhg"), 4),
         6
     );
     assert_eq!(
-        detect_signal(String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")),
+        detect_signal(String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 4),
         10
     );
     assert_eq!(
-        detect_signal(String::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")),
+        detect_signal(String::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 4),
         11
     );
 }
 fn main() -> Result<(), Box<dyn Error>> {
-    let answer = detect_signal(read_to_string("src/input.txt")?);
+    let answer = detect_signal(read_to_string("src/input.txt")?, 4);
     println!("Answer Part#1 {answer}");
 
     assert_eq!(answer, 1850);
